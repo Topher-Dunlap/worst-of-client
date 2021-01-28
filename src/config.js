36786ce16a922aeb0be1512a45ed7c205bac4891
topher-dunlap <thelonioustopher@yahoo.com>
2021-01-27 22:47:34 -0600

    updated API URL in config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api'
-    // API_ENDPOINT: 'https://agile-inlet-69276.herokuapp.com/api'
-    // local_endpoint: 'http://localhost:8000/api'
+    API_ENDPOINT: 'https://agile-inlet-69276.herokuapp.com/api'
+    // API_ENDPOINT: 'http://localhost:8000/api'
 }
