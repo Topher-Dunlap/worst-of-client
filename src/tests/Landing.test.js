c04fe0d90330ffd51e59ccda6a46bc98304c1c88
topher-dunlap <thelonioustopher@yahoo.com>
2021-01-30 12:43:11 -0600

    updating tests for Landing

--- src/tests/Landing.test.js
+++ src/tests/Landing.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Landing from '../components/Landing';
+import ThemeContext from "../components/ThemeContext";
 import renderer from 'react-test-renderer';
 
 describe(`Landing component`, () => {
@@ -11,18 +12,27 @@ describe(`Landing component`, () => {
         {backgroundColor: "#EEEEEE"},
     ]
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Landing/>, div);
-    ReactDOM.unmountComponentAtNode(div);
-})
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
 
-it('renders the UI as expected', () => {
-    const context = {backgroundColors}
-    const tree = renderer
-        .create(<Landing/>, context)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-})
+        ReactDOM.render(
+            <ThemeContext.Provider value={{backgroundColors}}>
+                <Landing/>,
+            </ThemeContext.Provider>,
+            div
+        );
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+            .create(
+                <ThemeContext.Provider value={{backgroundColors}}>
+                    <Landing/>,
+                </ThemeContext.Provider>,
+            )
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    })
 
 })
