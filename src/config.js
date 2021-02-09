f2c82d00ae63ec59b360514c7b7ce92a866049ce
topher-dunlap <thelonioustopher@yahoo.com>
2021-02-09 01:02:19 -0600

    updating pre build

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 export default {
-    API_ENDPOINT: 'https://agile-inlet-69276.herokuapp.com/api',
     // API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://agile-inlet-69276.herokuapp.com/api',
     TOKEN_KEY: 'worst-of-client-auth-token',
 }
